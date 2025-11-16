01bbe22fcfc0c824d121c22bfa909604da1f0a4f
jesshdzz <jesushernandezz.dev@gmail.com>
2025-11-15 22:18:38 -0600

    ADD: main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,49 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+module Main where
+
+import GameTypes
+import GameLogic
+import Data.Aeson (encode, decode)
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Maybe (fromMaybe)
 
 main :: IO ()
-main = someFunc
+main = do
+    -- Leer la entrada JSON desde stdin
+    input <- B.getContents
+    -- Decodificar la entrada JSON a una Peticion
+    let peticion = decode input :: Maybe Peticion
+
+    case peticion of
+        Nothing -> do
+            -- Si la decodificación falla, responder con un error
+            let errorResp = Respuesta Nothing "Error: JSON inválido" False
+            B.putStrLn (encode respuesta)
+        Just peticion -> do
+            procesarPeticion peticion
+
+procesarPeticion :: Peticion -> IO ()
+procesarPeticion p
+    | accion p == "mover" = manejarMovimiento p
+    | otherwise = do
+        let resp = Respuesta Nothing "Error: Acción desconocida" False
+        B.putStrLn (encode resp)
+
+manejarMovimiento :: Peticion -> IO ()
+manejarMovimiento p = do
+    let estadoActual = estado p
+        desde = fromMaybe (-1) (origen p)
+        hacia = fromMaybe (-1) (destino p)
+
+    if esMovimientoValido estadoActual desde hacia
+        then do
+            let nuevoEstado = moverBola estadoActual desde hacia
+                victoria = estaResuelto nuevoEstado
+                msg = if victoria then "Ganaste!" else "OK"
+                resp = Respuesta (Just nuevoEstado) msg victoria
+            B.putStrLn (encode resp)
+        else do
+            -- movimiento inválido
+            let resp = Respuesta Nothing "Movimiento inválido" False
+            B.putStrLn (encode resp)
