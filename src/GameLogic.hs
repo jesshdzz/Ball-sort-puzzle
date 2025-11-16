dec214c14bcb5254e783f265dc4bd4923069e1a1
jesshdzz <jesushernandezz.dev@gmail.com>
2025-11-15 21:27:39 -0600

    ADD: Add moverBola function in GameLogic

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -3,7 +3,7 @@ module GameLogic where
 import GameTypes
 
 tamanoTubo :: Int
-tamanoTubo = 4
+tamanoTubo = 4 -- Tamaño máximo de bolas en un tubo
 
 obtenerTubo :: EstadoJuego -> Int -> Maybe Tubo
 obtenerTubo estado indice
@@ -25,3 +25,28 @@ reglasValidas tuboOrigen tuboDestino
     | null tuboDestino = True -- Se puede mover a un tubo vacío
     otherwise =
         (head tuboOrigen) == (head tuboDestino)  -- Solo se puede mover si los colores coinciden
+
+-- Ejecuta el movimiento, devolviendo el nuevo estado del juego
+-- Se asume que el movimiento es válido
+moverBola :: EstadoJuego -> Int -> Int -> Maybe EstadoJuego
+moverBola estado origen destino
+    let
+        -- Sacar la bola del tubo de origen
+        tuboOrigen = estado !! origen
+        bolaAMover = head tuboOrigen
+        nuevoTuboOrigen = tail tuboOrigen
+
+        -- Agregar la bola al tubo de destino
+        tuboDestino = estado !! destino
+        nuevoTuboDestino = bolaAMover : tuboDestino
+        
+        -- Actualizar el estado del juego
+        -- Reemplaza el tubo en la posición dada con el nuevo tubo
+        actualizarLista :: Int -> a -> [a] -> [a]
+        actualizarLista idx nuevoElemento lista =
+            take idx lista ++ [nuevoElemento] ++ drop (idx + 1) lista
+        
+        -- Reemplaza primero el tubo de origen y luego el de destino
+        estadoIntermedio = actualizarLista origen nuevoTuboOrigen estado
+        estadoFinal = actualizarLista destino nuevoTuboDestino estadoIntermedio
+    in estadoFinal
