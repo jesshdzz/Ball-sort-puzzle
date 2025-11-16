9003183cd7e79f1dbc11b183b466cf03f374534b
jesshdzz <jesushernandezz.dev@gmail.com>
2025-11-15 22:09:02 -0600

    ADD: functions to communicate with python

--- src/GameTypes.hs
+++ src/GameTypes.hs
@@ -13,3 +13,20 @@ instance FromJSON Color
 
 type Tubo = [Color]
 type EstadoJuego = [Tubo]
+
+data Peticion = Peticion {
+    accion :: String,       -- "mover", "verificar", etc...
+    estado :: EstadoJuego,  -- El tablero actual
+    origen :: Maybe Int,    -- Puede ser null si no se requiere
+    destino :: Maybe Int    -- Puede ser null si no se requiere
+} deriving (Show, Generic)
+
+instance FromJSON Peticion
+
+data Respuesta = Respuesta {
+    nuevoEstado :: Maybe EstadoJuego, -- El tablero actualizado, si aplica
+    mensaje :: String,                -- "OK", "Movimiento inválido", etc...
+    esVictoria :: Bool                -- Indica si el juego se ha ganado
+} deriving (Show, Generic)
+
+instance ToJSON Respuesta
